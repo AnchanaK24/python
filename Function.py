845c843caa499d89ad2a76039c22e78186f28480
AnchanaK24 <74914989+AnchanaK24@users.noreply.github.com>
2020-11-27 22:02:06 +0530

    Functions

---
+++ Function.py
@@ -0,0 +1,8 @@
+def math(a,b):
+    print("addition of two numbers:",a+b)
+    print("subtraction of two numbers:",a-b)
+    print("multiplication of two numbers:",a*b)
+    print("divison of two numbers:",a//b)
+x=int(input("First number:" ))
+y=int(input("second number:"))
+math(x,y)
