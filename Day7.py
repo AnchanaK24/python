6a51e6b8443cf415cabec78dd0aa0128ebb4da8a
AnchanaK24 <74914989+AnchanaK24@users.noreply.github.com>
2020-11-29 19:47:41 +0530

    task 7

---
+++ Day7.py
@@ -0,0 +1,17 @@
+#ex1
+from IMP import mylist
+mylist()
+
+#ex2
+import pandas as pd
+data=[[1,2,3],[4,5,6]]
+res=pd.series(data)
+print(res)
+
+#ex3
+from ran import rand
+rand()
+
+#ex4
+import sys
+sys.path
